ea2bf3d0bd39c4b32b21602a5edd5a010f5e5eac
Lucas Ivisson <lucasivisson1@gmail.com>
2023-11-09 11:38:23 -0300

    🚧 working with headers

--- src/server.js
+++ src/server.js
@@ -6,11 +6,12 @@ const server = http.createServer((req, res) => {
   const { method, url } = req;
 
   if(method === 'GET' && url === '/users') {
-    return res.end('Listagem de usuários')
+    return res.setHeader('Content-type', 'application/json').end(JSON.stringify(users))
   }
 
   if(method === 'POST' && url === '/users') {
     users.push({
+      id: 1,
       name: 'John Doe',
       email: 'johndoe@example.com'
     })
